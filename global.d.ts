68c290c194fad20502cfc5b9230b2d343ab269a3
danikaze <danikaze@gmail.com>
2019-12-23 09:26:12 +0900

    Environment: Node + TypeScript + webpack (#1)
    - NodeJS app
    - Using TypeScript
    - Compiled with webpack
    - tslint + prettier
    - travis-ci

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+// Globals from constants.js
+declare const APP_VERSION: string;
+
+// Globals from webpack
+declare const IS_PRODUCTION: boolean;
+declare const __non_webpack_require__: NodeRequireFunction;
